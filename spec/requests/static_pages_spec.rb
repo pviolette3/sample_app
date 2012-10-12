11ab0b243eb434788e08512c5232f37bcf305dea
Patrick Violette <pviolette3@gatech.edu>
2012-10-12 19:44:03 -0400

    Got the static page rspec tests passing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,22 +5,38 @@ describe "StaticPages" do
 	describe "HomePage" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :h1 => 'Sample App')
+		end
+		
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
 		end
 	end
 	
 	describe "HelpPage" do
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text=>'Help')
+		end
+		
+		it "should have the title 'Help'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text=> "Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 	
 	describe "AboutPage" do
-		it "should have the content 'About'" do
+		it "should have the h1 'About'" do
 			visit '/static_pages/about'
-			page.should have_content('About')
+			page.should have_selector('h1', :text=>"About")
 		end
+		
+		it "should have the title 'About'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text=> "Ruby on Rails Tutorial Sample App | About")
+		end
+		
 	end
 		
 end
