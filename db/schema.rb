2668012ef8acf7cab1cf1c141504ccaba9cd80f8
Patrick Violette <pviolette3@gatech.edu>
2012-10-13 02:56:25 -0400

    added password column to user data base.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013064231) do
+ActiveRecord::Schema.define(:version => 20121013065504) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
