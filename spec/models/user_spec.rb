d9b8ea11513b7e602c6758dcbad9a635e625afec
Patrick Violette <pviolette3@gatech.edu>
2012-10-13 03:25:58 -0400

    Got the tests written, but they don't pass. for some reason, email validation is now broken.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ require 'spec_helper'
 
 describe User do
 	before(:each) do
-	  @user = User.new(name: "test", email: "test@examPLE.com")
+	  @user = User.new(name: "test", email: "test@examPLE.com", password: "pw", password_confirmation: "pw")
 	end
 	
 	subject {@user}
@@ -21,6 +21,9 @@ describe User do
 	it {should respond_to(:name)}
 	it {should respond_to(:email)}
 	it {should respond_to(:password_digest)}
+	it {should respond_to(:password)}
+	it {should respond_to(:password_confirmation)}
+	it {should respond_to(:authenticate)}
 	it {should be_valid}
 	
 	describe "when name is not present" do
@@ -70,5 +73,24 @@ describe User do
 
     it { should_not be_valid }
   end
-	
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
 end
