2fb2129ed2e081745ac98f164cd4b90035678332
Patrick Violette <pviolette3@gatech.edu>
2012-10-24 01:15:47 -0400

    user signup test spec is mostly done, now we have to make it pass.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
   
   def show
   	@user = User.find(params[:id])
   end
+  
+#  def create
+#  	User.create
+#  end
 end
